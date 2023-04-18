d35245ea3da61e3bc8133f19bad3a0870b60a456
VIIProgramm <vlad1.1gordeev@gmail.com>
2023-04-18 19:36:15 +0300

    Progect_js_6

---
+++ dist/script.js
@@ -0,0 +1,4 @@
+var rad = parseFloat(prompt('Введите радиус окружности'));
+alert('Площадь данной окружности = ' + Math.PI * Math.pow(rad, 2));
+var side = parseFloat(prompt('Введите длинну стороны квадрата'));
+alert('Периметр данного квадрата = ' + side * 4);
---
+++ script.ts
@@ -0,0 +1,5 @@
+let rad = parseFloat(prompt('Введите радиус окружности') as string)
+alert('Площадь данной окружности = '+  Math.PI*rad**2)
+
+let side = parseFloat(prompt('Введите длинну стороны квадрата') as string)
+alert('Периметр данного квадрата = '+  side*4)
